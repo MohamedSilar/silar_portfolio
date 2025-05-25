94bdb804bd264b4575838fb3de997a037d981c34
MohamedSilar <mohamedsilar26@gmail.com>
2025-05-25 18:59:26 +0530

    vite config changed

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/silar_portfolio/',  // 👈 Must match your GitHub repo name
+  base: '/',  // 👈 Must match your GitHub repo name
   plugins: [react()],
 })
