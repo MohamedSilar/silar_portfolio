be5a152a7c3b7d1ab1825d553fa5defb4d84de14
M MOHAMED SILAR <143829598+MohamedSilar@users.noreply.github.com>
2025-12-16 08:44:28 +0530

    Update project image path in Projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -20,7 +20,7 @@ const Projects: React.FC = () => {
   id: 1,
   title: "MedKit – Digital Healthcare Chatbot",
   description: "Developed a modular healthcare chatbot using Zoho SalesIQ, implementing multiple conversational workflows for appointments, payments, wellness, and user assistance. Integrated 20+ APIs for real-time scheduling, OTP verification, payments, notifications, and health data, ensuring secure and scalable automation.",
-  image: "/images/MED.png",
+  image: "/images/medd.png",
   tags: ["Zoho SalesIQ", "Deluge", "APIs", "JSON", "Healthcare", "Automation"],
   github: "https://github.com/MohamedSilar/Medkit",
   live: "https://medkitt.netlify.app/"
